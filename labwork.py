74ad87ec990aac58a61ad5ee3bbceeeb20c6f529
beastx03 <114742075+beastx03@users.noreply.github.com>
2022-10-03 12:45:17 +0100

    Add files via upload
    task1-4 of portfolio 2

---
+++ labwork.py
@@ -0,0 +1,29 @@
+# Task 1
+name = input ("What's your name? : ")
+print("Hello", name,". Welcome to the program")
+print("_______________________________________________")
+
+# Task 2
+celsius = input("Please enter a temperature in celsius: ")
+print("Converting celsius to fahrenheit...")
+fahrenheit = (float(celsius)*9/5 + 32)
+print (fahrenheit)
+print("_______________________________________________")
+
+# Task 3
+student = int(input("How many students? : "))
+group = int(input("How many students per group? : "))
+pergroup = student//group
+leftover = student%group
+if leftover < 2:
+    print("There will be", pergroup,"groups and", leftover,"student left over.")
+else:
+    print("There will be", pergroup,"groups and", leftover,"students left over.")
+print("_______________________________________________")
+
+# Task 4
+candy = int(input("What is the number of candy? : "))
+students = int(input("What is the number of students? : "))
+giveout = candy//students
+leftovers = candy%students
+print("Give", giveout, "to each student. You will have", leftovers,"candy left over.")
